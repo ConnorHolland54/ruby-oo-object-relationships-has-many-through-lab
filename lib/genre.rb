a27326faf6495c824159dac4e7a0ec9454545283
Connor Holland <supersoccergoalie123@gmail.com>
2020-12-15 17:01:39 -0700

    commit tree 581816f67f9baa0c8fd3d2d83f25332cdfa87daf

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,25 @@
 
+class Genre
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select { |song| song.genre == self}
+    end
+
+    def artists
+        arr = Song.all.select { |song| song.genre == self}
+        arr.collect { |song| song.artist}
+    end
+
+
+end
